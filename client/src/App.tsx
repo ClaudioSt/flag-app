01fcccf562d84fa3ee60da37eb6887b84ddb1fd8
ClaudioSt <claudio@stanullo.de>
2025-03-20 18:34:41 +0100

    api und backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
+
+  const [array, setArray] = useState([]);
+
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8080/api");
+    setArray(response.data.offense);
+    console.log(response.data.offense)
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, [])
 
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-500">
         Hello world!
       </h1>
+      {array.map((offense, index) => (
+        <div key={index}>
+          <p>{offense}
+
+          </p>
+          <br>
+          </br>
+        </div>
+
+      ))}
     </>
   )
 }
